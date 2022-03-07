f47e8bf125d345bd1b7ab7735f55f325b97bb00f
PredaaA <46051820+PredaaA@users.noreply.github.com>
2022-03-07 01:09:54 +0100

    Merge branch 'master' of https://github.com/Rapptz/discord.py into v2-martine

--- build/lib/discord/http.py
+++ build/lib/discord/http.py
@@ -263,7 +263,7 @@ def handle_message_parameters(
 
 
 class Route:
-    BASE: ClassVar[str] = 'https://discord.com/api/v9'
+    BASE: ClassVar[str] = 'https://discord.com/api/v10'
 
     def __init__(self, method: str, path: str, **parameters: Any) -> None:
         self.path: str = path
@@ -1984,9 +1984,9 @@ class HTTPClient:
         except HTTPException as exc:
             raise GatewayNotFound() from exc
         if zlib:
-            value = '{0}?encoding={1}&v=9&compress=zlib-stream'
+            value = '{0}?encoding={1}&v=10&compress=zlib-stream'
         else:
-            value = '{0}?encoding={1}&v=9'
+            value = '{0}?encoding={1}&v=10'
         return value.format(data['url'], encoding)
 
     async def get_bot_gateway(self, *, encoding: str = 'json', zlib: bool = False) -> Tuple[int, str]:
@@ -1996,9 +1996,9 @@ class HTTPClient:
             raise GatewayNotFound() from exc
 
         if zlib:
-            value = '{0}?encoding={1}&v=9&compress=zlib-stream'
+            value = '{0}?encoding={1}&v=10&compress=zlib-stream'
         else:
-            value = '{0}?encoding={1}&v=9'
+            value = '{0}?encoding={1}&v=10'
         return data['shards'], value.format(data['url'], encoding)
 
     def get_user(self, user_id: Snowflake) -> Response[user.User]:
